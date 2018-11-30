839f091150b595847231aad5f1b3c0196258a97e
Marius <mariusz1306+github@gmail.com>
2018-11-30 16:30:32 +0100

    Działa

--- app/src/main/java/io5/rss/MainActivity.java
+++ app/src/main/java/io5/rss/MainActivity.java
@@ -1,37 +1,79 @@
 package io5.rss;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
-public class MainActivity extends FragmentActivity {
+import com.google.code.rome.android.repackaged.com.sun.syndication.feed.synd.SyndFeed;
 
-    private TextView Text;
-    private int fails;
+
+public class MainActivity extends Activity {
+    // This is the feed that will be parsed
+    String url = "https://wszczecinie.pl/rss/news.xml";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Text = (TextView) findViewById(R.id.Text);
-        String url = "https://wszczecinie.pl/rss/news.xml";
-        try {
-            RSSFetcher test = new RSSFetcher(url);
-            while(true) {
-                test.run();
-                if (test.isDownloaded())
-                    Text.setText(test.getFeed().getAuthor());
-                else {
-                    String text = "Failed to download " + ++fails + " times";
-                    Text.setText(text);
-                }
-                System.out.print("");
-            }
-        } catch (Exception e) {
-            String text = "Failed to download " + ++fails + " times";
-            Text.setText(text);
-        }
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setTitle( "Reddit /r/GameDeals" );
+
+        initList newsList = new initList();
+        newsList.setContext(this);
+        newsList.execute();
     }
 
+    private View createList(SyndFeed feed, Activity activity)
+    {
+        LinearLayout mainPanel = new LinearLayout( activity );
+        ListView listView = new ListView( activity );
+        final FeedListAdapter feedListAdapter = new FeedListAdapter(activity, feed );
+        listView.setOnItemClickListener( new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick( AdapterView<?> parentView, View childView, int position, long id )
+            {
+                feedListAdapter.click( position );
+            }
+        } );
+        listView.setAdapter( feedListAdapter );
+        listView.setBackgroundColor(Color.WHITE);
+        listView.setCacheColorHint(Color.TRANSPARENT);
+        mainPanel.addView( listView );
+        return mainPanel;
+    }
 
+    private class initList extends AsyncTask<String, Integer, String>
+    {
+        private SyndFeed feed;
+        private Activity context;
+
+        @Override
+        protected String doInBackground(String... params)
+        {
+            RssAtomFeedRetriever rssAtomFeedRetriever = new RssAtomFeedRetriever();
+            feed = rssAtomFeedRetriever.getMostRecentNews(url);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+            super.onPostExecute(s);
+            setContentView(createList(feed, getContext()));
+        }
+
+        private void setContext(Activity context)
+        {
+            this.context = context;
+        }
+
+        private Activity getContext()
+        {
+            return context;
+        }
+    }
 }
