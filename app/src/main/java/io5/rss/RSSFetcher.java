d2848fe274dd284418cd8526e7f3db54aea9d024
Marius <mariusz1306+github@gmail.com>
2018-11-30 16:35:34 +0100

    Działa

--- app/src/main/java/io5/rss/RSSFetcher.java
+++ app/src/main/java/io5/rss/RSSFetcher.java
@@ -1,9 +1,14 @@
 package io5.rss;
 
+import android.util.Xml;
+
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,20 +20,25 @@ public class RSSFetcher implements Runnable {
 
     private final String stringUrl;
     private final URL feedUrl;
+    private final InputStream inputStream;
     private SyndFeed feed;
 
-    public RSSFetcher(String stringUrl) throws MalformedURLException{
+    public RSSFetcher(String stringUrl, InputStream inputStream) throws MalformedURLException{
         this.stringUrl = stringUrl;
         this.feedUrl = new URL(stringUrl);
+        this.inputStream = inputStream;
     }
 
     @Override
     public void run() {
         try {
             SyndFeedInput input = new SyndFeedInput();
-            this.feed = input.build(new XmlReader(feedUrl));
+            //File file = getAssets().open("filename.txt");
+            XmlReader xml = new XmlReader(getInputStream());
+            System.out.println();
+            this.feed = input.build(xml);
         } catch (Exception e){
-
+            System.out.println(e.getMessage());
         }
     }
 
@@ -42,6 +52,10 @@ public class RSSFetcher implements Runnable {
 
     public URL getFeedUrl() {
         return feedUrl;
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
     }
 
     public SyndFeed getFeed() {
